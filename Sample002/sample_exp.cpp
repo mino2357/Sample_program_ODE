a7606e9385036da3b903dfb42cc75d1c6abc88ef
mino2357 <mino2357@gmail.com>
2017-06-21 00:44:29 +0900

    Sample001はまだ書きかけ．とりあえずSample002にRK4の標準的なコードを書いた

--- Sample002/sample_exp.cpp
+++ Sample002/sample_exp.cpp
@@ -2,7 +2,7 @@
  * dy/dt = y
  * y(0) = y_init
  *
- * をオイラー法で解くよ．
+ * を古典的ルンゲ・クッタ法で解くよ．
  *
  * みーくん氏
  */
@@ -25,15 +25,24 @@ int main(){
     //常微分方程式の初期条件を設定するよ．
     double y = y_init;
 
+    //RK法で使うkたちを宣言して初期化するよ．
+    double k1, k2, k3, k4;
+    k1 = k2 = k3 = k4 = 0.0;
+
     //はじめの値を表示するよ.
     std::cout << t << " " << y_init << std::endl;
 
     //漸化式を解くよ．T_limit秒まで解くよ．
     for(int i=1; t<T_limit; i++){
+        //k_i(i=1,2,3,4)の値を求めるよ．
+        k1 = func(y);
+        k2 = func(y + dt * k1 / 2.0);
+        k3 = func(y + dt * k2 / 2.0);
+        k4 = func(y + dt * k3);
         //dt秒後のyの値を求めるよ．
-        y = y + dt * func(y);
+        y = y + dt / 6. * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
         //時間tを進めるよ．
-        t = i * dt;
+        t += dt;
         //t秒後のときのyの値を表示するよ．
         std::cout << t << " " << y << std::endl;
     }
