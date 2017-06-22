ea9c8419a73f41c813af83a2e134f0c46b3533ce
mino2357 <mino2357@gmail.com>
2017-06-23 08:39:23 +0900

    add

--- Sample002/sample_logistic.cpp
+++ Sample002/sample_logistic.cpp
@@ -2,17 +2,19 @@
  * dy/dt = alpha * (1 - y / beta) y
  * y(0) = y_init
  *
- * を古典的ルンゲ・クッタ法で解くよ．
+ * を古典的ルンゲ・クッタ法で解く．
  *
  * みーくん氏
  */
 
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 //パラメータ
 const double y_init  =  0.1;
-const double dt      =  0.01; //時間の刻み幅だよ．
+const double dt      =  0.001; //時間の刻み幅．
 const double T_limit = 10.0;
 const double alpha   =  1.0;
 const double beta    =  1.0;
@@ -22,30 +24,32 @@ double func(double y){
 }
 
 int main(){
-    //t=0からはじめるよ．
+    std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+        
+     //t=0からはじめる．
     double t = 0.0;
-    //常微分方程式の初期条件を設定するよ．
+    //常微分方程式の初期条件を設定．
     double y = y_init;
 
-    //RK法で使うkたちを宣言して初期化するよ．
+    //RK法で使うkたちを宣言して初期化．
     double k1, k2, k3, k4;
     k1 = k2 = k3 = k4 = 0.0;
 
-    //はじめの値を表示するよ.
-    std::cout << t << " " << y_init << std::endl;
+    //はじめの値を表示.
+    std::cout << t << " " << y_init << " " << std::abs(y - 1.0 / (1.0 + 9.0 * std::exp(-t))) << std::endl;
 
-    //漸化式を解くよ．T_limit秒まで解くよ．
+    //漸化式を解くよ．T_limit秒まで解く．
     for(int i=1; t<T_limit; i++){
-        //k_i(i=1,2,3,4)の値を求めるよ．
+        //k_i(i=1,2,3,4)の値を求める．
         k1 = func(y);
         k2 = func(y + dt * k1 / 2.0);
         k3 = func(y + dt * k2 / 2.0);
         k4 = func(y + dt * k3);
-        //dt秒後のyの値を求めるよ．
+        //dt秒後のyの値を求める．
         y = y + dt / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
-        //時間tを進めるよ．
+        //時間tをdt秒進める．
         t = i * dt;
-        //t秒後のときのyの値を表示するよ．
-        std::cout << t << " " << y << std::endl;
+        //t秒後のときのyの値を表示する．
+        std::cout << t << " " << y << " "<< std::abs(y - 1.0 / (1.0 + 9.0 * std::exp(-t))) << std::endl;
     }
 }
