a5baaa8763ed74180db43895170a359f5dbb7dee
mino2357 <mino2357@gmail.com>
2017-06-23 09:44:09 +0900

    add log10

--- Sample002/sample_logistic.cpp
+++ Sample002/sample_logistic.cpp
@@ -14,7 +14,7 @@
 
 //パラメータ
 const double y_init  =  0.1;
-const double dt      =  0.1; //時間の刻み幅．
+const double dt      =  0.001; //時間の刻み幅．
 const double T_limit = 10.0;
 const double alpha   =  1.0;
 const double beta    =  1.0;
@@ -39,7 +39,7 @@ int main(){
 
     //はじめの値を表示. 「時刻t シミュレーション結果 相対誤差の対数」の順で出力．
     C2 = 1.0 - beta / y_init;
-    std::cout << t << " " << y_init << " " << std::log(std::abs(y - beta / (1.0 - C2 * std::exp(- alpha * t))) / std::abs(beta / (1.0 - C2 * std::exp(- alpha * t)))) << std::endl;
+    std::cout << t << " " << y_init << " " << std::log10(std::abs(y - beta / (1.0 - C2 * std::exp(- alpha * t))) / std::abs(beta / (1.0 - C2 * std::exp(- alpha * t)))) << std::endl;
 
     //漸化式を解くよ．T_limit秒まで解く．
     for(int i=1; t<T_limit; i++){
@@ -53,6 +53,6 @@ int main(){
         //時間tをdt秒進める．
         t = i * dt;
         //t秒後のときのyの値を表示する．
-        std::cout << t << " " << y << " "<< std::log(std::abs(y - beta / (1.0 - C2 * std::exp(- alpha * t))) / std::abs(beta / (1.0 - C2 * std::exp(- alpha * t)))) << std::endl;
+        std::cout << t << " " << y << " "<< std::log10(std::abs(y - beta / (1.0 - C2 * std::exp(- alpha * t))) / std::abs(beta / (1.0 - C2 * std::exp(- alpha * t)))) << std::endl;
     }
 }
