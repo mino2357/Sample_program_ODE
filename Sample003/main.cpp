e10b2872f096601b097776524f243aeb8007804c
mino2357 <mino2357@gmail.com>
2017-07-09 17:46:20 +0900

    add Sample003. Euler method.

--- Sample003/main.cpp
+++ Sample003/main.cpp
@@ -1,14 +1,24 @@
+/*
+ * Euler法で二階の常微分方程式を解く．
+ * テーマ：単振動．調和振動子．
+ *
+ * by みーくん．
+ */
+
 #include <iostream>
 #include "myvector.hpp"
 
+// パラメータ
 constexpr double k      = 1.;
 constexpr double m      = 1.;
 constexpr double x_init = 1.;
 constexpr double v_init = 0.;
 
+//時刻に関するパラメータ
 constexpr double dt      = 0.001;
 constexpr double t_limit = 20.0;
 
+//R^2からR^2への関数．
 mino2357::vector<> func(mino2357::vector<>& x){
     return mino2357::vector<>{x.getComponentY(), - k / m * x.getComponentX()};
 }
@@ -20,7 +30,7 @@ int main(){
 
     for(size_t i {}; t<t_limit; ++i){
         std::cout << t << " " << x.getComponentX() << " " << x.getComponentY() << std::endl;
-        
+        //Euler法で常微分方程式を解く．       
         x = x + dt * func(x);
         t = i * dt;
     }
