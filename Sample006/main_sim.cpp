b5576abc9c637d8bfb1c437989e1e8a03c188b00
mino2357 <mino2357@gmail.com>
2017-08-05 23:56:01 +0900

    fix and set gnuplot's point size

--- Sample006/main_sim.cpp
+++ Sample006/main_sim.cpp
@@ -35,14 +35,15 @@ constexpr double t_limit     = 20000.0;
 //インターバル
 constexpr int INTV = 3;
 
-//R^2からR^2への関数．
-Eigen::Matrix<double, 4, 1> func(const Eigen::Matrix<double, 4, 1>& x){
-	double theta1 = x(0, 0);
-	double eta1   = x(1, 0);
-	double theta2 = x(2, 0);
-	double eta2   = x(3, 0);
+//R^4からR^4への関数．
+template <typename T = double>
+Eigen::Matrix<T, 4, 1> func(const Eigen::Matrix<double, 4, 1>& x){
+	T theta1 = x(0, 0);
+	T eta1   = x(1, 0);
+	T theta2 = x(2, 0);
+	T eta2   = x(3, 0);
 
-	return Eigen::Matrix<double, 4, 1> {
+	return Eigen::Matrix<T, 4, 1> {
 		eta1,
 		(- m1 * g * std::sin(theta1) - m2 * (g * std::sin(theta1) + L2 * eta2 * eta2 * std::sin(theta1 - theta2) + (L1 * eta1 * eta1 * std::sin(theta1 - theta2) - g * std::sin(theta2)) * std::cos(theta1 - theta2))) / (L1 * (m1 + m2 * (std::sin(theta1 - theta2) * (std::sin(theta1 - theta2))))),
 		eta2,
@@ -72,10 +73,7 @@ int main(){
     Eigen::Matrix<double, 4, 1> x(theta1_init, omega1_init, theta2_init, omega2_init);
 	std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1);
 
-	Eigen::Matrix<double ,4, 1> k1;
-	Eigen::Matrix<double ,4, 1> k2;
-	Eigen::Matrix<double ,4, 1> k3;
-	Eigen::Matrix<double ,4, 1> k4;
+	Eigen::Matrix<double ,4, 1> k1, k2, k3, k4;
 
     double t{};
     
@@ -92,7 +90,7 @@ int main(){
 
 		//描画
         if(i%INTV == 0){
-            fprintf(gp, "plot '-' w l linewidth 3\n");
+            fprintf(gp, "plot '-' w lp lw 3 pt 7 ps 3\n");
             fprintf(gp, "0.0 0.0\n");
             fprintf(gp, "%f %f\n", L1 * std::sin(x(0, 0)), - L1 * std::cos(x(0,0)));
             fprintf(gp, "%f %f\n", L1 * std::sin(x(0, 0)) + L2 * std::sin(x(2,0)), - L1 * std::cos(x(0, 0)) - L2 * std::cos(x(2, 0)));
@@ -101,10 +99,10 @@ int main(){
         }
 
         //RK4法で常微分方程式を解く．
-        k1 = func(x);
-        k2 = func(x + dt / 2. * k1);
-        k3 = func(x + dt / 2. * k2);
-        k4 = func(x + dt * k3);
+        k1 = func<>(x);
+        k2 = func<>(x + dt / 2. * k1);
+        k3 = func<>(x + dt / 2. * k2);
+        k4 = func<>(x + dt * k3);
         x = x + dt / 6. * (k1 + 2. * k2 + 2. * k3 + k4);
         t = i * dt;
     }
