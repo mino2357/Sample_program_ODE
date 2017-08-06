fc469026dbcb4e46ab71370ff9a4339a65c4503c
mino2357 <mino2357@gmail.com>
2017-08-06 16:02:22 +0900

    create png images

--- Sample006/main_sim.cpp
+++ Sample006/main_sim.cpp
@@ -10,19 +10,24 @@
 #include <cmath>
 #include <limits>
 #include <iomanip>
+#include <string>
+
+#include <cstring>
+#include <memory>
 
 #include <Eigen/Core>
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
 
+#include <stdlib.h>
 // パラメータ
 constexpr double g           = 1.0;//9.8
 constexpr double m1          = 1.0;
 constexpr double m2          = 1.0;
 constexpr double L1          = 1.0;
-constexpr double L2          = 0.4;
+constexpr double L2          = 0.6;
 //初期角
-constexpr double theta1_init = 2.0;
+constexpr double theta1_init = 2.5;
 constexpr double theta2_init = 2.0;
 //初角速度
 constexpr double omega1_init = 0.;
@@ -33,7 +38,7 @@ constexpr double dt          =     0.001;
 constexpr double t_limit     = 20000.0;
 
 //インターバル
-constexpr int INTV = 3;
+constexpr int INTV = 40;
 
 //R^4からR^4への関数．
 template <typename T = double>
@@ -76,25 +81,49 @@ int main(){
 	Eigen::Matrix<double ,4, 1> k1, k2, k3, k4;
 
     double t{};
-    
+
+	std::string dir_name = "L1-" + std::to_string(L1);
+	dir_name += "-L2-" + std::to_string(L2);
+	dir_name += "-m1-" + std::to_string(m1);
+	dir_name += "-m2-" + std::to_string(m2);
+	dir_name += "-g-" + std::to_string(g);
+	dir_name += "-theta1_init-" + std::to_string(theta1_init);
+	dir_name += "-theta2_init-" + std::to_string(theta2_init);
+
+	char c_dir_name[dir_name.size() + 1];
+	std::strcpy(c_dir_name, dir_name.c_str());
+	
+	std::string str =  "mkdir -p " + dir_name;
+
+	char com_dir[str.size() + 1];
+	std::strcpy(com_dir, str.c_str());
+	//system
+	if(system(com_dir)){
+		std::cout << dir_name + "was not created." << std::endl;
+	}
+
 	// gnuplot
 	FILE *gp = popen("gnuplot -persist","w");
     fprintf(gp, "set xr [-2.1:2.1]\n");
     fprintf(gp, "set yr [%f:%f]\n", -2.1, 2.1);
     fprintf(gp, "set size square\n");
     fprintf(gp, "set grid\n");
+    fprintf(gp, "unset key\n");
+    fprintf(gp, "set term png\n");
 
     for(std::size_t i{}; t<t_limit; ++i){
         //std::cout << t << " " << x(0, 0) << " " << x(1, 0) << " " << x(2, 0) << " " << x(3, 0) << " " << energy<>(x) << std::endl;
-        std::cout << t << " " << kineticEnergy<>(x) << " " << potentialEnergy<>(x) << " " << kineticEnergy<>(x) + potentialEnergy<>(x) << std::endl;
 
 		//描画
         if(i%INTV == 0){
+        	std::cout << t << " " << kineticEnergy<>(x) << " " << potentialEnergy<>(x) << " " << kineticEnergy<>(x) + potentialEnergy<>(x) << std::endl;
+            fprintf(gp, "set output '%s/%06d.png'\n", c_dir_name, static_cast<int>(i/INTV));
             fprintf(gp, "plot '-' w lp lw 3 pt 7 ps 3\n");
             fprintf(gp, "0.0 0.0\n");
             fprintf(gp, "%f %f\n", L1 * std::sin(x(0, 0)), - L1 * std::cos(x(0,0)));
             fprintf(gp, "%f %f\n", L1 * std::sin(x(0, 0)) + L2 * std::sin(x(2,0)), - L1 * std::cos(x(0, 0)) - L2 * std::cos(x(2, 0)));
             fprintf(gp, "e\n");
+            fprintf(gp, "set output\n");
             fflush(gp);
         }
 
