3577f226f9e66439e8b558736416cff7b9bce69b
mino2357 <mino2357@gmail.com>
2017-08-06 16:11:37 +0900

    fix

--- Sample006/main_sim.cpp
+++ Sample006/main_sim.cpp
@@ -43,67 +43,67 @@ constexpr int INTV = 40;
 //R^4からR^4への関数．
 template <typename T = double>
 Eigen::Matrix<T, 4, 1> func(const Eigen::Matrix<double, 4, 1>& x){
-	T theta1 = x(0, 0);
-	T eta1   = x(1, 0);
-	T theta2 = x(2, 0);
-	T eta2   = x(3, 0);
+    T theta1 = x(0, 0);
+    T eta1   = x(1, 0);
+    T theta2 = x(2, 0);
+    T eta2   = x(3, 0);
 
-	return Eigen::Matrix<T, 4, 1> {
-		eta1,
-		(- m1 * g * std::sin(theta1) - m2 * (g * std::sin(theta1) + L2 * eta2 * eta2 * std::sin(theta1 - theta2) + (L1 * eta1 * eta1 * std::sin(theta1 - theta2) - g * std::sin(theta2)) * std::cos(theta1 - theta2))) / (L1 * (m1 + m2 * (std::sin(theta1 - theta2) * (std::sin(theta1 - theta2))))),
-		eta2,
-		((m1 + m2) * (L1 * eta1 * eta1 * std::sin(theta1 - theta2) - g * std::sin(theta2) + g * std::sin(theta1) * std::cos(theta1 - theta2)) + m2 * L2 * eta2 * eta2 * std::cos(theta1 - theta2) * std::sin(theta1 - theta2)) / (L2 * (m1 + m2 * std::sin(theta1 - theta2) * std::sin(theta1 - theta2)))
-	};
+    return Eigen::Matrix<T, 4, 1> {
+        eta1,
+        (- m1 * g * std::sin(theta1) - m2 * (g * std::sin(theta1) + L2 * eta2 * eta2 * std::sin(theta1 - theta2) + (L1 * eta1 * eta1 * std::sin(theta1 - theta2) - g * std::sin(theta2)) * std::cos(theta1 - theta2))) / (L1 * (m1 + m2 * (std::sin(theta1 - theta2) * (std::sin(theta1 - theta2))))),
+        eta2,
+        ((m1 + m2) * (L1 * eta1 * eta1 * std::sin(theta1 - theta2) - g * std::sin(theta2) + g * std::sin(theta1) * std::cos(theta1 - theta2)) + m2 * L2 * eta2 * eta2 * std::cos(theta1 - theta2) * std::sin(theta1 - theta2)) / (L2 * (m1 + m2 * std::sin(theta1 - theta2) * std::sin(theta1 - theta2)))
+    };
 }
 
 template <typename T = double>
 T potentialEnergy(Eigen::Matrix<T, 4, 1>& x){
-	T theta1 = x(0, 0);
-	T eta1   = x(1, 0);
-	T theta2 = x(2, 0);
-	T eta2   = x(3, 0);
+    T theta1 = x(0, 0);
+    T eta1   = x(1, 0);
+    T theta2 = x(2, 0);
+    T eta2   = x(3, 0);
 
-	return 0.5 * m1 * L1 * L1 * eta1 * eta1 + 0.5 * m2 * (L1 * L1 * eta1 * eta1 + L2 * L2 * eta2 * eta2 + 2. * L1 * L2 * eta1 * eta2 * std::cos(theta1 - theta2));
+    return 0.5 * m1 * L1 * L1 * eta1 * eta1 + 0.5 * m2 * (L1 * L1 * eta1 * eta1 + L2 * L2 * eta2 * eta2 + 2. * L1 * L2 * eta1 * eta2 * std::cos(theta1 - theta2));
 }
 
 template <typename T = double>
 T kineticEnergy(Eigen::Matrix<T, 4, 1>& x){
-	T theta1 = x(0, 0);
-	T theta2 = x(2, 0);
+    T theta1 = x(0, 0);
+    T theta2 = x(2, 0);
 
-	return - m1 * g * L1 * std::cos(theta1) - m2 * g * (L1 * std::cos(theta1) + L2 * std::cos(theta2));
+    return - m1 * g * L1 * std::cos(theta1) - m2 * g * (L1 * std::cos(theta1) + L2 * std::cos(theta2));
 }
 
 int main(){
     Eigen::Matrix<double, 4, 1> x(theta1_init, omega1_init, theta2_init, omega2_init);
-	std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+    std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1);
 
-	Eigen::Matrix<double ,4, 1> k1, k2, k3, k4;
+    Eigen::Matrix<double ,4, 1> k1, k2, k3, k4;
 
     double t{};
 
-	std::string dir_name = "L1-" + std::to_string(L1);
-	dir_name += "-L2-" + std::to_string(L2);
-	dir_name += "-m1-" + std::to_string(m1);
-	dir_name += "-m2-" + std::to_string(m2);
-	dir_name += "-g-" + std::to_string(g);
-	dir_name += "-theta1_init-" + std::to_string(theta1_init);
-	dir_name += "-theta2_init-" + std::to_string(theta2_init);
+    std::string dir_name = "L1-" + std::to_string(L1);
+    dir_name += "-L2-" + std::to_string(L2);
+    dir_name += "-m1-" + std::to_string(m1);
+    dir_name += "-m2-" + std::to_string(m2);
+    dir_name += "-g-" + std::to_string(g);
+    dir_name += "-theta1_init-" + std::to_string(theta1_init);
+    dir_name += "-theta2_init-" + std::to_string(theta2_init);
 
-	char c_dir_name[dir_name.size() + 1];
-	std::strcpy(c_dir_name, dir_name.c_str());
-	
-	std::string str =  "mkdir -p " + dir_name;
+    char c_dir_name[dir_name.size() + 1];
+    std::strcpy(c_dir_name, dir_name.c_str());
+    
+    std::string str =  "mkdir -p " + dir_name;
 
-	char com_dir[str.size() + 1];
-	std::strcpy(com_dir, str.c_str());
-	//system
-	if(system(com_dir)){
-		std::cout << dir_name + "was not created." << std::endl;
-	}
+    char com_dir[str.size() + 1];
+    std::strcpy(com_dir, str.c_str());
+    //system
+    if(system(com_dir)){
+        std::cout << dir_name + "was not created." << std::endl;
+    }
 
-	// gnuplot
-	FILE *gp = popen("gnuplot -persist","w");
+    // gnuplot
+    FILE *gp = popen("gnuplot -persist","w");
     fprintf(gp, "set xr [-2.1:2.1]\n");
     fprintf(gp, "set yr [%f:%f]\n", -2.1, 2.1);
     fprintf(gp, "set size square\n");
@@ -114,9 +114,9 @@ int main(){
     for(std::size_t i{}; t<t_limit; ++i){
         //std::cout << t << " " << x(0, 0) << " " << x(1, 0) << " " << x(2, 0) << " " << x(3, 0) << " " << energy<>(x) << std::endl;
 
-		//描画
+        //描画
         if(i%INTV == 0){
-        	std::cout << t << " " << kineticEnergy<>(x) << " " << potentialEnergy<>(x) << " " << kineticEnergy<>(x) + potentialEnergy<>(x) << std::endl;
+            std::cout << t << " " << kineticEnergy<>(x) << " " << potentialEnergy<>(x) << " " << kineticEnergy<>(x) + potentialEnergy<>(x) << std::endl;
             fprintf(gp, "set output '%s/%06d.png'\n", c_dir_name, static_cast<int>(i/INTV));
             fprintf(gp, "plot '-' w lp lw 3 pt 7 ps 3\n");
             fprintf(gp, "0.0 0.0\n");
