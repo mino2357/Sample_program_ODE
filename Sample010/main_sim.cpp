d51265a2e2a0fe26d4aec46e2d60ed81560a45a7
mino2357 <mino2357@gmail.com>
2017-08-11 20:00:37 +0900

    add まだまだ修正が必要

--- Sample010/main_sim.cpp
+++ Sample010/main_sim.cpp
@@ -24,19 +24,14 @@ namespace mp = boost::multiprecision;
 using multiFloat = mp::cpp_dec_float_100;
 
 // パラメータ
-const multiFloat e("0.99999999");
-//初期角
-const multiFloat x_init("3.0");
-const multiFloat y_init("3.0");
-//初角速度
-const multiFloat x_dot_init("0.0");
-const multiFloat y_dot_init("0.0");
+const multiFloat e("0.9999");
 
 //時刻に関するパラメータ
-multiFloat dt("1.0e-20");
-const multiFloat t_limit("20.0");
+multiFloat dt("1.0e-4");
+const multiFloat t_limit("10.0");
 
-const multiFloat e_tol("10e-10");
+const multiFloat R_Tol("10e-5");
+const multiFloat A_Tol("10e-5");
 const multiFloat t_min("10e-50");
 const multiFloat t_max("0.1");
 
@@ -74,10 +69,10 @@ int main(){
     multiFloat t{};
 
     for(std::size_t i{}; t<t_limit; ++i){
-    
+        
         if(i%INTV == 0){
-            std::cout << t << " " << mp::log10(dt) << " " << x(0, 0) << " " << x(1, 0) << " " << x(2, 0) << " " << x(3, 0) << std::endl;
         }
+        
 
         //RK4法で常微分方程式を解く．
         k1 = func<>(x);
@@ -91,39 +86,31 @@ int main(){
         x5 = x + dt * (static_cast<multiFloat>(16)/135 * k1 + static_cast<multiFloat>(6656)/12825 * k3 + static_cast<multiFloat>(28561)/56430 * k4 - static_cast<multiFloat>(9)/50 * k5 + static_cast<multiFloat>(2)/55 * k6);
        
         auto temp  = (static_cast<multiFloat>(25)/216 * k1 + static_cast<multiFloat>(1408)/2565 * k3 + static_cast<multiFloat>(2197)/4104 * k4 - static_cast<multiFloat>(1)/5 * k5) - (static_cast<multiFloat>(16)/135 * k1 + static_cast<multiFloat>(6656)/12825 * k3 + static_cast<multiFloat>(28561)/56430 * k4 - static_cast<multiFloat>(9)/50 * k5 + static_cast<multiFloat>(2)/55 * k6);
-        auto R = mp::sqrt(temp(0, 0) * temp(0, 0) + temp(1, 0) * temp(1, 0) + temp(2, 0) * temp(2, 0) + temp(3, 0) * temp(3, 0));
+        multiFloat R = mp::sqrt((temp(0, 0) * temp(0, 0) + temp(1, 0) * temp(1, 0) + temp(2, 0) * temp(2, 0) + temp(3, 0) * temp(3, 0)));
 
-        auto delta = static_cast<multiFloat>("0.86") * static_cast<multiFloat>(mp::pow(static_cast<multiFloat>(e_tol/(2 * R)), static_cast<multiFloat>("0.25")));
+        multiFloat x4_abs = mp::sqrt(static_cast<multiFloat>(x4(0, 0) * x4(0,0) + x4(1, 0) * x4(1,0) + x4(2, 0) * x4(2,0) + x4(3, 0) * x4(3,0)));
 
-        //この辺は適当にチューニング．
-        
-        if(delta < static_cast<multiFloat>("0.1")){
-            dt = static_cast<multiFloat>("0.1") * dt;
-            if(dt < t_min){
-                dt = t_min;
-            }
-        }else if(delta > 4){
-            dt = 4 * dt;
-            if(dt > static_cast<multiFloat>("0.1")){
-                dt = t_max;
-            }
-        }else{
-            dt = delta * dt;
-            if(dt > static_cast<multiFloat>("0.1")){
-                dt = t_max;
-            }
-            if(dt < t_min){
-                dt = t_min;
-            }
+        multiFloat E = static_cast<multiFloat>(1)/static_cast<multiFloat>(2) * (A_Tol + x4_abs * R_Tol);
+
+
+
+        if(mp::pow(E/R, static_cast<multiFloat>(1)/5) < 1){
+            dt = dt * 0.6 * mp::pow(E/R, static_cast<multiFloat>(1)/5);
+            continue;
         }
 
-        //std::cout << dt << std::endl;
-
         x  = x4;
+        //std::cout << t << " " << mp::log10(dt) << " " << x(0, 0) << " " << x(1, 0) << " " << x(2, 0) << " " << x(3, 0) << std::endl;
+        std::cout << t << " " << mp::log10(dt) << " " << R << " " << mp::pow(E/R, static_cast<multiFloat>(1)/5) << " " << x(0, 0) << " " << x(2, 0) << " " <<mp::sqrt(static_cast<multiFloat>(x4(1, 0) * x4(1,0) + x4(3, 0) * x4(3,0))) << std::endl;
 
         t += dt;
 
         //std::cout << x4 << std::endl;
+        
+        dt = 10.0 * dt;
+        if(dt > t_max){
+            dt = t_max;
+        }
     }
     
     //pclose(gp);
